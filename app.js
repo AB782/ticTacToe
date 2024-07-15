d7e0cef9035ccb19c42505b95c23aa461bc22aee
AB782 <aayushbansal782@gmail.com>
2024-07-16 01:06:43 +0530

    added reset and new btn functionality

--- app.js
+++ app.js
@@ -16,9 +16,14 @@ const winPatterns = [
     [6,7,8],
 ];
 
+const resetGame = () =>{
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
 boxes.forEach((box) => {
     box.addEventListener("click",()  => {
-        console.log("box was clicked");
         if(turnO){
             box.innerText = "O";
             turnO = false;
@@ -34,6 +39,20 @@ boxes.forEach((box) => {
 const showWinner = (winner) =>{
     msg.innerText = `Congratulations, Winner is ${winner}`;
     msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+const disableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
 }
 const checkWinner = () =>{
     for(let pattern of winPatterns){
@@ -43,9 +62,11 @@ const checkWinner = () =>{
 
         if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
             if(pos1Val == pos2Val && pos2Val == pos3Val){
-                console.log("winner",pos1Val);
                 showWinner(pos1Val);
             }
         }
     }
 };
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
